94040f4e1691af72cac308de8247854680bb3394
117503445 <t117503445@gmail.com>
2024-05-12 17:06:26 +0000

    feat: provider
    Signed-off-by: 117503445 <t117503445@gmail.com>

--- pkg/translator/translator.go
+++ pkg/translator/translator.go
@@ -1,12 +1,16 @@
 package translator
 
+import "github.com/117503445/markdown-translate/internal/provider"
+
 type Translator struct {
+	provider provider.Provider
 }
 
 func NewTranslator() *Translator {
-	return &Translator{}
+	provider := provider.NewMockProvider()
+	return &Translator{provider: provider}
 }
 
 func (t *Translator) Translate(source string) string {
-	return source
+	return t.provider.Translate(source)
 }
